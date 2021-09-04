f0f59ac0c08d7606f19728d038bd58d2457d4c3c
Erin Quinn <equinn125@gmail.com>
2021-09-04 15:55:22 -0600

    add dependent delete all in models

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ApplicationRecord
-  has_many :application_pets
+  has_many :application_pets, dependent: :delete_all
   has_many :pets, through: :application_pets
   validates :name, presence: true
   validates :address, presence: true
