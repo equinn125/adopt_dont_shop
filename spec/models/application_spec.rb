1b422b48632bc5c5b8e1cfaa81a8715fb2cad1dc
Erin Quinn <equinn125@gmail.com>
2021-09-04 15:42:57 -0600

    Add incomplete application impl and tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Application, tyoe: :model do
     it { should have_many :application_pets}
     it {should have_many(:pets).through(:application_pets)}
   end
+
+  describe "validators" do
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:address)}
+    it { should validate_presence_of(:city)}
+    it { should validate_presence_of(:state)}
+    it { should validate_presence_of(:zip)}
+  end
 end
